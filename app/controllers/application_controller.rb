eeea87806bc324488d5c6e8e9ef986b679a7f52f
Karla R <karla.y.reyes12@gmail.com>
2018-08-07 15:53:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,35 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    erb :index
   end
+  
+  get '/posts/new'
+  erb :new
+  end
+  
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+  
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  
+  get '/posts/:id'
+  @post = Post.find_by_id(params[:id])
+  erb :show
+  end
+  
+  get '/posts/:id/edit'
+  @post = Post.find_by_id(params[:id])
+  @post.name = params[:name]
+  @post.content = params[:content]
+  @post.save
 end
+
+
+
+
